22a53488fe0624624fa4e96aeaf60dcb9087f8df
Venkat <venkat@Venkats-MacBook-Air.local>
2024-12-21 15:26:58 +0200

    creating components and getting ready with appwrite setup

--- app/page.jsx
+++ app/page.jsx
@@ -1,7 +1,15 @@
+import rooms from '../data/rooms.json';
+import RoomCard from '@/components/RoomCard';
+import Heading from '@/components/Heading';
 export default function Home() {
   return (
     <>
-  <h1>Book it app</h1>
+    <Heading title='Available Rooms' />
+      {rooms.length > 0 ? (
+        rooms.map((room) => <RoomCard key={room.$id} room={room} /> )
+      ) : (
+        <p>No rooms available at the moment</p>
+      )}
     </>
   );
 }
